0448e3720c09a8115264d3a67639561374d9b9f0
Rishi <sec20cs070@sairamtap.edu.in>
2022-10-02 00:15:38 +0530

    Dice Roller

---
+++ app/src/main/java/com/rishi/diceroller/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.rishi.diceroller
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            rollDice()
+
+        }
+    }
+        private  fun rollDice()
+        {
+            val dice = Dice(6)
+            val diceroll = dice.roll()
+            val diceImage: ImageView = findViewById(R.id.imageView)
+            when(diceroll)
+            {
+                1 -> diceImage.setImageResource(R.drawable.dice_1)
+                2 -> diceImage.setImageResource(R.drawable.dice_2)
+                3 -> diceImage.setImageResource(R.drawable.dice_3)
+                4 -> diceImage.setImageResource(R.drawable.dice_4)
+                5 -> diceImage.setImageResource(R.drawable.dice_5)
+                6 -> diceImage.setImageResource(R.drawable.dice_6)
+
+            }
+            diceImage.contentDescription = diceroll.toString()
+
+        }
+
+    }
+class Dice(private  val numSide: Int)
+{
+    fun roll() : Int
+    {
+        return (1..numSide).random()
+    }
+}
